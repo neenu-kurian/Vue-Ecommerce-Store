71456b660429caa0a1aaffc6fcc35a274a7c7331
Neenu Kurian <nkurian@salmon.com>
2021-06-06 22:17:12 +0200

    cart page

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -4,8 +4,8 @@
       <img v-bind:src="product.imageUrl" />
     </div>
     <div id="product-details">
-      <h1>{{ product.name}}</h1>
-      <h3 id="price">${{product.price}}</h3>
+      <h1>{{ product.name }}</h1>
+      <h3 id="price">${{ product.price }}</h3>
       <p>Average Rating: {{ product.averageRating }}</p>
       <button id="add-to-cart">Add to Cart</button>
       <h4>Description</h4>
@@ -18,50 +18,50 @@
 import { products } from '../fake-data';
 
 export default {
-  name:'ProductDetailPage',
+  name: 'ProductDetailPage',
   data() {
     return {
-      product: products.find((p)=>p.id === this.$route.params.id)
-    }
-  }
+      product: products.find((p) => p.id === this.$route.params.id),
+    };
+  },
 };
 </script>
 
 <style scoped>
-  #page-wrap {
-    margin-top: 16px;
-    padding: 16px;
-    max-width: 600px;
-  }
+#page-wrap {
+  margin-top: 16px;
+  padding: 16px;
+  max-width: 600px;
+}
 
-  #img-wrap {
-    text-align: center;
-  }
+#img-wrap {
+  text-align: center;
+}
 
-  img {
-    width: 400px;
-  }
+img {
+  width: 400px;
+}
 
-  #product-details {
-    padding: 16px;
-    position: relative;
-  }
+#product-details {
+  padding: 16px;
+  position: relative;
+}
 
-  #add-to-cart {
-    width: 100%;
-  }
+#add-to-cart {
+  width: 100%;
+}
 
-  #price {
-    position: absolute;
-    top: 24px;
-    right: 16px;
-  }
+#price {
+  position: absolute;
+  top: 24px;
+  right: 16px;
+}
 
-  .green-button {
-    background-color: green;
-  }
+.green-button {
+  background-color: green;
+}
 
-  .grey-button {
-    background-color: #888;
-  }
+.grey-button {
+  background-color: #888;
+}
 </style>
