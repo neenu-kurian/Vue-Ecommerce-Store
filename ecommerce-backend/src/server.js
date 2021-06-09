374cd2bf5a850255bca0a9bac3b1e0c4dd933bc6
Neenu Kurian <nkurian@salmon.com>
2021-06-09 12:41:58 +0200

    added db get method

--- ecommerce-backend/src/server.js
+++ ecommerce-backend/src/server.js
@@ -1,5 +1,6 @@
 import  express from "express";
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 
 export const products = [{
   id: '123',
@@ -66,7 +67,7 @@ export const products = [{
   averageRating: '5.0',
 },
 {
-  id: '901',
+  id: '903',
   name: 'Teal Dress Shoes',
   price: '330.00',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
@@ -74,14 +75,14 @@ export const products = [{
   averageRating: '5.0',
 },
 {
-  id: '789',
+  id: '771',
   name: 'Fancy Boots',
   price: '230.00',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
   imageUrl: '/images/shoes-11.jpg',
   averageRating: '5.0',
 }, {
-  id: '890',
+  id: '899',
   name: 'Gold Shoes',
   price: '180.00',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
@@ -99,23 +100,62 @@ export let cartItems = [
 const app=express();
 app.use(bodyParser.json());
 
-app.get('/api/products',(req,res)=>{
+app.get('/api/products',async(req,res)=>{
+  const client = await MongoClient.connect(
+    'mongodb://127.0.0.1:27017',
+    {
+      useNewUrlParser: true,useUnifiedTopology:true
+    }
+  );
+  const db=client.db('vue-db');
+  const products = await db.collections('products').find({}).toArray();
 	res.status(200).json(products);
+  client.close();
 });
 
-app.get('/api/users/:userId/cart',(req,res)=>{
-	res.status(200).json(cartItems);
+app.get('/api/users/:userId/cart',async(req,res)=>{
+  const { userId } = req.params;
+  const client = await MongoClient.connect(
+    'mongodb://127.0.0.1:27017',
+    {
+      useNewUrlParser: true,useUnifiedTopology:true
+    }
+  );
+  const db=client.db('vue-db');
+  const user = await db.collections('users').findOne({id: userId});
+  if(!user) return res.status(404).json('Couldnt find user');
+  const products = await db.collections('products').find({}).toArray();
+  const cartItemIds = user.cartItems;
+
+  const cartItems = cartItemIds.map(cartItemId=>{
+    products.find(eachproduct=>eachproduct.id===cartItemId);
+  });
+
+  res.status(200).json(cartItems);
+  client.close();
+	
 });
 
-app.get('/api/products/:productId',(req,res)=>{
+app.get('/api/products/:productId',async(req,res)=>{
+
+  const client = await MongoClient.connect(
+    'mongodb://127.0.0.1:27017',
+    {
+      useNewUrlParser: true,useUnifiedTopology:true
+    }
+  );
+  const db=client.db('vue-db');
+
 	const { productId } = req.params;
-	const product =  products.find((product)=>(product.id === productId));
+  const product = await db.collections('products').find({id: productId});
+
 	if(product) {
 		res.status(200).json(product);
 	}
 	else{
 		res.status(404).json("couldnot find the product");
 	}
+  client.close();
 });
 
 app.post('/api/users/:userId/cart',(req,res)=>{
